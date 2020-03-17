5cdda42d0f7d6e2977f932979fed8c0a95b04c91
Paul Hammant <paul@hammant.org>
2020-03-17 19:29:29 +0000

    fab

--- sed_command.sh
+++ sed_command.sh
@@ -1,4 +1,4 @@
 wget --mirror --convert-links --adjust-extension --page-requisites --no-parent https://WearAFuckingMask.com
 rm -rf img/ sfw/
 mv wearafuckingmask.com/* .
-sed -i.bu 'wearafuckingmask-' ./index.html && sed -i.bu 's/stay-the-fuck-at-home/stay-in-your-fabulous-home/g' ./index.html && sed -i.bu 's#img/#fabulous_images/#g' ./index.html && sed -i.bu 's/Fucking/Fabulous/g' ./index.html && sed -i.bu 's/Fuck/Fabulous/g' ./index.html && sed -i.bu 's/larrysalibra/charltonaustin/g' ./index.html
+sed -i.bu 's/wearafuckingmask-/wearafabmask-/g' ./index.html && sed -i.bu 's/stay-the-fuck-at-home/stay-in-your-fabulous-home/g' ./index.html && sed -i.bu 's#img/#fabulous_images/#g' ./index.html && sed -i.bu 's/Fucking/Fabulous/g' ./index.html && sed -i.bu 's/Fuck/Fabulous/g' ./index.html && sed -i.bu 's/larrysalibra/charltonaustin/g' ./index.html
