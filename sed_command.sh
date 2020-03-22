8309c9a607ae7f4cf205b79cc880a2253d50a91f
Paul Hammant <paul@hammant.org>
2020-03-22 13:22:56 +0000

    tweaks

--- sed_command.sh
+++ sed_command.sh
@@ -1,4 +1,17 @@
 wget --mirror --convert-links --adjust-extension --page-requisites --no-parent https://WearAFuckingMask.com
 rm -rf img/ sfw/
 mv wearafuckingmask.com/* .
-sed -i.bu 's/wearafuckingmask-/wearafabmask-/g' ./index.html && sed -i.bu 's/staythefuckhome/stay-in-your-fabulous-home/g' ./index.html && sed -i.bu 's#img/#fabulous_images/#g' ./index.html && sed -i.bu 's/Fucking/Fabulous/g' ./index.html && sed -i.bu 's/Fuck/Fabulous/g' ./index.html && sed -i.bu 's/larrysalibra/charltonaustin/g' ./index.html
+sed -i.bu 's/wearafuckingmask-/wearafabmask-/g' ./index.html 
+sed -i.bu 's/staythefuckhome/stay-in-your-fabulous-home/g' ./index.html 
+sed -i.bu 's#img/#fabulous_images/#g' ./index.html 
+sed -i.bu 's/Fucking/Fabulous/g' ./index.html 
+sed -i.bu 's/Fuck/Fabulous/g' ./index.html
+sed -i.bu 's/larrysalibra/charltonaustin/g' ./index.html
+sed -i.bu 's/StayTheFabulousHome/StayInMyFabulousHome/g' ./index.html
+sed -i.bu 's/Fabulous virus/Un-fabulous virus/g' ./index.html
+sed -i.bu 's/Fabulous infected/Un-fabulous infected/g' ./index.html
+sed -i.bu 's/Fabulous flu/Un-fabulous flu/g' ./index.html
+sed -i.bu 's/Fabulous cough/Un-fabulous cough/g' ./index.html
+sed -i.bu 's/Fabulous sneeze/Un-fabulous sneeze/g' ./index.html
+sed -i.bu 's/Fabulous risk/Un-fabulous risk/g' ./index.html
+sed -i.bu 's/Fabulous those Fabulous Fabulousers, they Fabulous let this Fabulous/Ignore them, they let this Un-fabulous/' ./index.html
